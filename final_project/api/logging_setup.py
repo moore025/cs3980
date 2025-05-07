16d994a575a59cee3b1474be8bfd1eeb73234b46
jthomas46 <jacobthomas@me.com>
2025-05-07 16:42:33 -0500

    Finished tests

--- final_project/api/logging_setup.py
+++ final_project/api/logging_setup.py
@@ -1,11 +1,12 @@
 from logging import INFO, StreamHandler, basicConfig
 from logging.handlers import TimedRotatingFileHandler
 
+
 def setup_logging():
-    file_log = TimedRotatingFileHandler("./logs/app.log", when="d", interval=1)
+    file_log = TimedRotatingFileHandler("api/logs/app.log", when="d", interval=1)
     console_log = StreamHandler()
     basicConfig(
-        level=INFO, 
-        format="%(asctime)s %(levelname).4s %(name)s:%(lineno)d %(message)s", 
+        level=INFO,
+        format="%(asctime)s %(levelname).4s %(name)s:%(lineno)d %(message)s",
         handlers=[file_log, console_log],
-        )
+    )
